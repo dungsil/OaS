e77a5e9061eb4468c1f835fbe79f7c066362a935
김용건 (Kim Younggeon) <mail@kyg.kr>
2022-10-25 02:27:33 +0900

    fix: 폰트를 Gothic A1으로 변경

--- pages/api/consumer59/[title].tsx
+++ pages/api/consumer59/[title].tsx
@@ -5,11 +5,6 @@ export const config = {
   runtime: 'experimental-edge',
 }
 
-const pretendard = {
-  medium: fetch(new URL('../../../assets/Pretendard-Medium.otf', import.meta.url)).then(res => res.arrayBuffer()),
-  black: fetch(new URL('../../../assets/Pretendard-Black.otf', import.meta.url)).then(res => res.arrayBuffer()),
-}
-
 export default async function handler(req: NextRequest) {
   const url = new URL(req.url || '/', `https://${req.headers.get('host')}`)
   const pathChunks = url.pathname.split('/')
@@ -26,19 +21,18 @@ export default async function handler(req: NextRequest) {
         width: '100%',
         height: '100%',
         backgroundColor: 'white',
-        fontFamily: 'Pretendard Medium',
-        fontFeatureSettings: '"case", "tnum", "ss05", "ss06"',
+        fontFamily: 'Gothic A1',
       }}>
         <div tw="flex flex-col justify-between items-center gap-16 text-center">
-          <p tw="font-900 text-gray-900 text-4xl" style={{ fontFamily: 'Pretendard Black' }}>
-            <span tw="text-5xl mr-4">"</span>
+          <p tw="font-900 text-gray-900 text-4xl">
+            <span tw="mr-4">"</span>
 
             {title}
 
-            <span tw="text-4xl ml-4">"</span>
+            <span tw="ml-4">"</span>
           </p>
 
-          <p tw="text-blue-800">
+          <p tw="font-500 text-blue-800">
             #멈춰라SPC #SPC불매 #소비자59
           </p>
         </div>
@@ -46,17 +40,17 @@ export default async function handler(req: NextRequest) {
         <div tw="absolute bottom-4 flex justify-between w-full px-4 py-0 text-gray-600 text-xs">
           <p tw="m-0">
             소비자59는
-            <b tw="mx-1" style={{ fontFamily: 'Pretendard Black' }}>
+            <b tw="font-700 mx-1">
               파리바게뜨노동자 힘내라 공동행동
             </b>
             및
-            <b tw="ml-1" style={{ fontFamily: 'Pretendard Black' }}>
+            <b tw="font-700 ml-1">
               화섬식품노조 파리바게뜨지회
             </b>
             와는 관련없는 사이트임을 밝힙니다.
           </p>
 
-          <p tw="m-0">
+          <p tw="font-300 m-0">
             🕊️ 소비자59 | c59.dun.land
           </p>
         </div>
@@ -68,8 +62,7 @@ export default async function handler(req: NextRequest) {
       emoji: 'fluent',
       debug: false,
       fonts: [
-        { name: 'Pretendard Medium', style: 'normal', data: await pretendard.medium },
-        { name: 'Pretendard Black', style: 'normal', data: await pretendard.black },
+        { name: 'Gothic A1', style: 'normal', weight: 900, data: await fetch(new URL('../../../assets/GothicA1-Black.ttf', import.meta.url)).then(res => res.arrayBuffer()) },
       ]
     }
   )
